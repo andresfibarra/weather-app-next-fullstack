110c4956b2e3d2ccd2015f169b11f9bb000a5087
andresfibarra <andres.f.ibarra.24@dartmouth.edu>
2025-11-24 18:17:40 -0800

    Adding file tree

---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+'use client'
+
+import React from 'react'; 
+import Logo from './Logo'
+
+function NavBar() {
+  return (
+    <header>
+      <div>WeatherApp</div>
+
+      <nav>
+        <Logo />
+      </nav>
+    </header>
+  )
+}
