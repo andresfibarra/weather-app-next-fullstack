039cf3770234f960b218a390c515b33edfdac1ea
andresfibarra <andres.f.ibarra.24@dartmouth.edu>
2025-11-30 17:57:10 -0800

    Initial version of DELETE endpoint

--- src/app/api/locations/[id]/route.js
+++ src/app/api/locations/[id]/route.js
@@ -1,6 +1,42 @@
+// src/app/api/locations/[id]/route.js
+
+import { NextResponse } from 'next/server';
+import { supabase } from '@/utils/supabase/client';
+
 // DELETE request to delete a location from the user's saved lists
-// in /api/locations/[id]/route.js
-/*
-1. Remove from user_saved_locations
-2. do I cascade down the other id's?
-*/
+export async function DELETE(request, { params }) {
+  console.log('DELETE /api/locations/[id] received');
+  const { id } = await params;
+
+  console.log('locationId:', id);
+  const { user_id } = await request.json();
+
+  if (!user_id) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: 'User ID is required',
+      },
+      { status: 400 },
+    );
+  }
+
+  const { error } = await supabase
+    .from('user_saved_locations')
+    .delete()
+    .eq('user_id', user_id)
+    .eq('location_id', id);
+
+  if (error) {
+    return NextResponse.json({ success: false, error: error.message }, { status: 500 });
+  }
+
+  return NextResponse.json(
+    {
+      success: true,
+      message: 'Location removed successfully',
+      deletedLocationId: id,
+    },
+    { status: 200 },
+  );
+}
