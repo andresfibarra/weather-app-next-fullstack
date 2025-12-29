e3305e015c5437032cf8a58d4a4a0baf66abb820
andresfibarra <andres.f.ibarra.24@dartmouth.edu>
2025-12-28 18:53:24 -0800

    Adding shadcn to login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,35 +1,61 @@
 // src/app/auth/login/page.jsx
 import { login, signup } from './actions';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+  Field,
+  FieldGroup,
+  FieldSet,
+  FieldDescription,
+  FieldSeparator,
+} from '@/components/ui/field';
 
 export default function LoginPage() {
   return (
     <form className="flex flex-col gap-4 w-2xl mx-auto border border-slate-300 rounded-md p-4">
-      <label htmlFor="email" className="text-lg font-bold text-slate-400">
-        Email:
-      </label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        className="border border-slate-300 rounded-md p-2"
-        required
-      />
-      <label htmlFor="password" className="text-lg font-bold text-slate-400">
-        Password:
-      </label>
-      <input
-        id="password"
-        name="password"
-        type="password"
-        className="border border-slate-300 rounded-md p-2"
-        required
-      />
-      <button formAction={login} className="bg-gray-800 text-white rounded-md p-2">
-        Log in
-      </button>
-      <button formAction={signup} className="bg-gray-800 text-white rounded-md p-2">
-        Sign up
-      </button>
+      <Field className="w-max-content">
+        <FieldSet className="text-xl font-bold">Welcome!</FieldSet>
+        <FieldDescription className="text-md">
+          Please enter your email and password to proceed.
+        </FieldDescription>
+        <FieldSeparator />
+        <FieldGroup>
+          <Field>
+            <Label htmlFor="email" className="text-md font-bold">
+              Email:
+            </Label>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              className="border border-slate-300 rounded-md p-2"
+              required
+            />
+          </Field>
+          <Field>
+            <Label htmlFor="password" className="text-md font-bold">
+              Password:
+            </Label>
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              className="border border-slate-300 rounded-md p-2"
+              required
+            />
+          </Field>
+
+          <Field>
+            <Button variant="outline" formAction={login} className="text-md">
+              Log in
+            </Button>
+            <Button variant="secondary" formAction={signup} className="text-md">
+              Sign up
+            </Button>
+          </Field>
+        </FieldGroup>
+      </Field>
     </form>
   );
 }
