54e707744ec11abcd4bf8b88fef5fcd4236b108f
andresfibarra <andres.f.ibarra.24@dartmouth.edu>
2025-12-29 21:15:05 -0800

    Adding signout route and linking to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,11 +2,15 @@
 
 import React from 'react';
 import Logo from './logo';
+import { Button } from '@/components/ui/button';
 
 function NavBar() {
   return (
     <header className="fixed top-0 left-0 w-full h-16 px-8 flex items-center justify-between border-b border-slate-800 backdrop-blur z-50">
       <Logo />
+      <form action="/auth/signout" method="post">
+        <Button variant="ghost">Sign Out</Button>
+      </form>
     </header>
   );
 }
