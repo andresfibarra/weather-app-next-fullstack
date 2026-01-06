578a76f0be61530d23459516bd0fb763b77add1a
andresfibarra <andres.f.ibarra.24@dartmouth.edu>
2026-01-06 10:51:28 -0800

    Adding redirect to login on no auth when attempting to access /weather

--- src/app/weather/layout.js
+++ src/app/weather/layout.js
@@ -1,3 +1,18 @@
-export default function WeatherLayout({ children }) {
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
+
+export default async function WeatherLayout({ children }) {
+  const supabase = await createClient();
+
+  // check for authenticated user to access weather page
+  const {
+    data: { user },
+    error: authError,
+  } = await supabase.auth.getUser();
+  if (!user || authError) {
+    console.error('Error getting authenticated user when calling WeatherLayout:', authError);
+    redirect('/login');
+  }
+
   return <>{children}</>;
 }
